80d2ab684f1c8766ed828c7e7cca343b1d6f6ede
dw <dw@example.com>
2012-12-20 18:02:23 +0800

    wiki

--- themes/basic/views/wiki/show.php
+++ themes/basic/views/wiki/show.php
@@ -12,8 +12,8 @@ $this->contentTitle = $model->title;
 
 <div class='wikiButtons'>
     <p class="row buttons">
-        <?php echo CHtml::link('Edit Page', array('update', 'id'=>$model->id));?>
-        <?php echo CHtml::link('Delete Page', array('class'=>'button'));?>
+        <input class="button" name="editpage" type="button" value="Edit Page" onclick=location.href="<?php echo Yii::app()->createUrl('wiki/update', array('id'=>$model->id)); ?>">
+        <input class="button" name="deletepage" type="button" value="Delete Page" onclick=location.href="<?php echo Yii::app()->createUrl('wiki/delete', array('id'=>$model->id)); ?>">
     </p>
 </div>
 
